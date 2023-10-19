bb1e9976dbbbf6998a6af3e3da94b7aac04f23eb
githugro <dsohugo@gmail.com>
2023-10-19 20:47:32 -0300

    Update JGDIFF.py

--- JGDIFF.py
+++ JGDIFF.py
@@ -3,6 +3,7 @@ import csv
 import datetime
 livros=[]
 pessoas=[]
+emprestimos=[]
 
 def cadastrar_livro(livros):
     livro=input("Digite o nome do livro:  ")
@@ -84,7 +85,57 @@ def Cadastrar_emprestimos(emprestimos):
 
 
 #Githug-o
+def editar_livros():
+    leitor = csv.DictReader(open('arquivo_livros.csv', mode="r"))
+    editlivro = input("Digite o nome do livro que deseja editar: ")
 
+    for livro in leitor:
+        if livro['Titulo'] == editlivro:
+            livro['Titulo'] = input("Título: ")
+            livro['Editora'] = input("Editora: ")
+            livro['Autor'] = input("Autor: ")
+        else:
+            print("Livro não encontrado.")
+    livros = leitor
+    criar_livro_csv()
+
+def editar_contatos():
+    leitor = csv.DictReader(open('arquivo_livros.csv', mode="r"))
+    editcontato = input("Digite o email do contato que deseja editar: ")
+
+    for contato in leitor:
+        if contato['Email'] == editcontato:
+            contato['Nome'] = input("Nome: ")
+            contato['Telefone'] = input("Telefone: ")
+            contato['Email'] = input("Email: ")
+        else:
+            print("Livro não encontrado.")
+    contatos = leitor
+    criar_livro_csv()
+
+def editar_emprestimos():
+    leitor = csv.DictReader(open('arquivo_livros.csv', mode="r"))
+    editcontato = input("Digite o nome do livro emprestado que deseja alterar: ")
+    opc1 = int(input("Qual informação deseja alterar?\n1 - Nome;\n2 - Telefone;\n3 - Email;\n4 - Todas.\n")) 
+
+    for emprestimo in leitor:
+        if emprestimo['Email'] == editcontato:
+            if opc1 == 1:
+                emprestimo['Nome'] = input("Nome: ")
+            elif opc1 == 2:
+                emprestimo['Telefone'] = input("Telefone: ")
+            elif opc1 == 3: 
+                emprestimo['Email'] = input("Email: ")         
+            elif opc1 == 4:             
+                emprestimo['Nome'] = input("Nome: ")
+                emprestimo['Telefone'] = input("Telefone: ")
+                emprestimo['Email'] = input("Email: ")
+            else:
+                print("Opção inválida!")
+        else:
+            print("Livro não encontrado.")
+    emprestimos = leitor
+    criar_livro_csv()
 
 
 
